61953c28567ac5b4ac0295f2b1e1e0087dd81da3
javiortig <dev.javiog@gmail.com>
2022-12-10 00:30:22 +0000

    pokemon details mobile margin hotfix

--- src/components/Pokemon/PokeCartasFisicas.js
+++ src/components/Pokemon/PokeCartasFisicas.js
@@ -1,34 +1,40 @@
 import React from "react";
 import { myPokemons } from "../../data/gen1";
-import {GetRazaDescubierta} from "../../functions/utils"
+import { GetRazaDescubierta } from "../../functions/utils"
 
 const PokeCartasFisicas = ({ name, types }) => {
   return (
     <div className="container-evolution mt-4">
       <h4 className="w-100 mb-4 section-title">Razas</h4>
-      <div className="carta-fisica-container">
+      <div className="">
         {
           myPokemons.map((item, index) => {
             //console.log(item.Nombre + '===' + name + ':' + (item.Nombre === name));
             if (item.Nombre.toLowerCase() === name) {
-              if (GetRazaDescubierta(item.Nombre.toLowerCase(), item.Raza.toLowerCase())){
+              if (GetRazaDescubierta(item.Nombre.toLowerCase(), item.Raza.toLowerCase())) {
                 return (
-                  <img className="carta-fisica"
-                    src={'/cartasFisicas/' + item.Nombre + '_' + item.Raza + '.png'}
-                    width={747 * 0.6}
-                    height={1038 * 0.6}
-                  />
+                  <div className="text-center">
+                    <img className="carta-fisica"
+                      src={'/cartasFisicas/' + item.Nombre + '_' + item.Raza + '.png'}
+                      width={747 * 0.35}
+                      height={1038 * 0.35}
+                    />
+                  </div>
                 );
               }
-              else{
-                return(<img
-                  //className="animation-up-down"
-                  src={"https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Question_mark_white_icon.svg/1200px-Question_mark_white_icon.svg.png"}
-                  width={747 * 0.6}
-                    height={1038 * 0.6}
-                />);
+              else {
+                return (
+                  <div className="text-center">
+                    <img
+                      className="animation-up-down text-center"
+                      src={"https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Question_mark_white_icon.svg/1200px-Question_mark_white_icon.svg.png"}
+                      width={747 * 0.35}
+                      height={1038 * 0.35}
+                    />
+                  </div>
+                );
               }
-              
+
             }
             else {
               return (<></>)
